f155db1ea66a730cbf6d78bb1e008eaa0a3d9dd6
Julian <git@julusian.co.uk>
2015-06-15 18:46:39 +0100

    Fix some bugs.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 //define the starting offset of the output. jump over the serial
 #define OUTPUT_START 2
 
-//define the starting camera to read info on
+//define the starting camera to read info on (n-1)
 #define CAMERA_START 1
 
 #define MAC_ADDRESS 0x90, 0xA2, 0xDA, 0x00, 0xF2, 0x00
--- tally.ino
+++ tally.ino
@@ -3,6 +3,9 @@
 #include <SPI.h>         // needed for Arduino versions later than 0018
 #include <Ethernet.h>
 
+#define OFF HIGH
+#define ON LOW
+
 
 // MAC address and IP address for this *particular* Ethernet Shield!
 // MAC address is printed on the shield
@@ -26,23 +29,23 @@ void setup() {
 
   // Start the Ethernet, Serial (debugging) and UDP:
   Ethernet.begin(mac,ip);
-  Serial.begin(9600);  
-  Serial.println("Serial started");
+  // Serial.begin(9600);  
+  // Serial.println("Serial started");
 
   Wire.begin(); // Start the wire library for communication with the GPIO chip.
 
   // Init:
-  for (int i=1; i<=8; i++)  {
+  for (int i=0; i<8; i++)  {
     pinMode(OUTPUT_START+i,OUTPUT);
   }
   // Set:
-  for (int i=1; i<=8; i++)  {
-    digitalWrite(OUTPUT_START+i, HIGH);
+  for (int i=0; i<8; i++)  {
+    digitalWrite(OUTPUT_START+i, ON);
     delay(100); 
   }
   // Clear:
-  for (int i=1; i<=8; i++)  {
-    digitalWrite(OUTPUT_START+i, LOW);
+  for (int i=0; i<8; i++)  {
+    digitalWrite(OUTPUT_START+i, OFF);
     delay(100); 
   }
 
@@ -55,32 +58,41 @@ void setup() {
 
 void setTallyProgramOutputs()  {
    // Setting colors of input select buttons:
-  for (uint8_t i=CAMERA_START;i<CAMERA_START+8;i++)  {
-    if (AtemSwitcher.getProgramTally(i))  {
-      digitalWrite(OUTPUT_START+i, HIGH);
+  for (uint8_t i=0;i<8;i++)  {
+    if (AtemSwitcher.getProgramTally(CAMERA_START+i))  {
+      digitalWrite(OUTPUT_START+i, ON);
     }       
     else {
-      digitalWrite(OUTPUT_START+i, LOW);
+      digitalWrite(OUTPUT_START+i, OFF);
     }
   }
 }
 
+int calcPinNum(int channel, bool program){
+  int num = OUTPUT_START+channel*2;
+  num += (program?0:1);
+  return num;
+}
+
+
 void setTallyPreviewProgramOutputs()  {
    // Setting colors of input select buttons:
-  for (uint8_t i=CAMERA_START;i<CAMERA_START+4;i++)  {
-    if (AtemSwitcher.getProgramTally(i))  {
-      digitalWrite(OUTPUT_START+i*2-1, HIGH);
-      Serial.print(i);
-      Serial.println("PGM");
+  for (uint8_t i=0;i<4;i++)  {
+    if (AtemSwitcher.getProgramTally(CAMERA_START+i))  {
+      digitalWrite(calcPinNum(i, true), ON);
+      digitalWrite(calcPinNum(i, false), OFF);
+      // Serial.print(i);
+      // Serial.println("PGM");
     }       
-    else if (AtemSwitcher.getPreviewTally(i) )  {
-      digitalWrite(OUTPUT_START+i*2, HIGH);
-      Serial.print(i);
-      Serial.println("PVW");
+    else if (AtemSwitcher.getPreviewTally(CAMERA_START+i) )  {
+      digitalWrite(calcPinNum(i, true), OFF);
+      digitalWrite(OUTPUT_START+i*2+1, ON);
+      // Serial.print(i);
+      // Serial.println("PVW");
     }       
     else {
-      digitalWrite(OUTPUT_START+i*2, LOW);
-      digitalWrite(OUTPUT_START+i*2-1, LOW);
+      digitalWrite(calcPinNum(i, true), OFF);
+      digitalWrite(calcPinNum(i, false), OFF);
     }
   }
 }
@@ -92,7 +104,7 @@ void loop() {
 
   // If connection is gone anyway, try to reconnect:
   if (AtemSwitcher.isConnectionTimedOut())  {
-     Serial.println("Connection to ATEM Switcher has timed out - reconnecting!");
+     // Serial.println("Connection to ATEM Switcher has timed out - reconnecting!");
      AtemSwitcher.connect();
   }  
 
